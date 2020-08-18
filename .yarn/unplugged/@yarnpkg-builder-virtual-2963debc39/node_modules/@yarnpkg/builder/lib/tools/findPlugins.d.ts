b20b97472cc31b29787b0c6b7e7b7d7911dc5718
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-08-18 21:36:41 +0200

    Unplug @yarnpkg/builder

---
+++ .yarn/unplugged/@yarnpkg-builder-virtual-2963debc39/node_modules/@yarnpkg/builder/lib/tools/findPlugins.d.ts
@@ -0,0 +1,5 @@
+export declare function findPlugins({ basedir, profile, plugins }: {
+    basedir: string;
+    profile: string;
+    plugins: Array<string>;
+}): string[];
---
+++ .yarn/unplugged/@yarnpkg-builder-virtual-2963debc39/node_modules/@yarnpkg/builder/lib/tools/isDynamicLib.d.ts
@@ -0,0 +1,1 @@
+export declare const isDynamicLib: (request: string) => boolean;
